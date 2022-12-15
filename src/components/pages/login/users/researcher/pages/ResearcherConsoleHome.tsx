9fa702d8fbdc767109693977441e8a6adb883e23
Andrew Stephens <asteph11@oldwestbury.edu>
2022-12-15 04:41:54 -0500

    Add chart.js and test components

--- src/components/pages/login/users/researcher/pages/ResearcherConsoleHome.tsx
+++ src/components/pages/login/users/researcher/pages/ResearcherConsoleHome.tsx
@@ -1,11 +1,23 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import './../../../../../../css/ConsoleHome.css';
 import {useNavigate} from "react-router-dom";
+import {BarChart, LineChart, PieChart} from "../components/Charts";
+import {UserData} from "../components/UserData";
+
 
 function ResearcherConsoleHome() {
 
     const [activePage, setActivePage] = useState(0);
     const navigate = useNavigate();
+
+    const [userData, setUserData] = useState({
+        labels: UserData.map((d:any)=>(d.year)),
+        datasets: [{
+            label: "Users Gained",
+            data: UserData.map((d:any)=>(d.userGain)),
+            backgroundColor: ["#051e35", "#0b2844", "#263e52"]
+        }]
+    });
 
     return (
         <Fragment>
@@ -23,6 +35,17 @@ function ResearcherConsoleHome() {
                             <label className={'page-bubble-label'}>Course Catalog</label>
                             <div className={'page-bubble-description'}>All basic information pertaining to courses,
                                 departments, and programs.</div>
+                        </div>
+                    </div>
+                    <div style={{display:"flex", marginTop:32, marginBottom:32}}>
+                        <div className={'page-bubble'} onClick={()=>navigate('./../catalog')}>
+                            <BarChart chartData={userData}/>
+                        </div>
+                        <div className={'page-bubble'} onClick={()=>navigate('./../catalog')}>
+                            <LineChart chartData={userData}/>
+                        </div>
+                        <div className={'page-bubble'} onClick={()=>navigate('./../catalog')}>
+                            <PieChart chartData={userData}/>
                         </div>
                     </div>
                 </div>
